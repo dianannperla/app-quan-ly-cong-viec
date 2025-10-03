1c8802a16974194035cb7835f23548bdadcf6bb3
dianannperla <waafirmelyza@hotmail.com>
2025-10-03 17:27:45 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskManager from './components/TaskManager';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Ứng Dụng Quản Lý Công Việc</h1>
+      <TaskManager />
+    </div>
+  );
+};
+
+export default App;
